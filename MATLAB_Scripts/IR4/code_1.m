babaecb466a894606bd4c35d0cc02c77a59d04c6
tejaprojects <teja1815251@gmail.com>
2017-05-05 01:18:38 +0530

    changes to script(.m) files

---
+++ MATLAB_Scripts/IR4/code_1.m
@@ -0,0 +1,26 @@
+clear
+clc
+
+folder = 'DataBase';
+
+file = fopen('images_list.txt', 'w');
+file_jpg = fopen('jpg_images_list.txt', 'w');
+
+for i=0:9
+    curr_dir = sprintf('%s\\%d', folder, i);
+    curr_dir_info = dir(sprintf('%s\\*.jpg', curr_dir));
+    num_images = size(curr_dir_info, 1);
+    for j = 1:num_images
+        img_file = sprintf('%s\\%s', curr_dir, curr_dir_info(j).name);
+        command = sprintf('md %s\\%d', curr_dir, j-1);
+        system(command);
+        command = sprintf('cp %s %s\\%d\\%d.jpg', img_file, curr_dir, j-1, j-1);
+        system(command);
+        command = sprintf('rm %s', img_file);
+        system(command);
+        fwrite(file, sprintf('%s\\%d\\%d\r\n', curr_dir, j-1, j-1));
+        fwrite(file_jpg, sprintf('%s\\%d\\%d.jpg\r\n', curr_dir, j-1, j-1));
+    end
+end
+fclose(file);
+fclose(file_jpg);
